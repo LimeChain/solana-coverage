9c0fb12f897bceaebf5841f584092314e2346b73
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2025-07-01 11:19:03 +0300

    initial commit - solana-coverage crate that can be used to provide the plumbing for getting code coverage from anchor and native solana programs

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(unexpected_cfgs)]
+
+#[allow(unused_imports)]
+pub use solana_program_stubs;
+
+#[macro_export]
+#[cfg(not(target_os = "solana"))]
+macro_rules! coverage {
+    ($entry_function:expr) => {
+        $crate::solana_program_stubs::declare_sol_app_stubs!($entry_function);
+    };
+}
+
+#[macro_export]
+#[cfg(not(target_os = "solana"))]
+macro_rules! anchor_coverage {
+    () => {
+        $crate::solana_program_stubs::declare_sol_app_stubs!(entry);
+    };
+}
