5e58249f58d29082f6f399ae74646e10846aebb1
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2025-07-17 13:12:23 +0300

    Stubsv2 coverage wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,19 @@ macro_rules! anchor_coverage {
         $crate::solana_program_stubs::declare_sol_app_stubs!(entry);
     };
 }
+
+#[macro_export]
+#[cfg(not(target_os = "solana"))]
+macro_rules! coveragev2 {
+    ($entry_function:expr) => {
+        $crate::solana_program_stubs::declare_sol_app_stubsv2!($entry_function);
+    };
+}
+
+#[macro_export]
+#[cfg(not(target_os = "solana"))]
+macro_rules! anchor_coveragev2 {
+    () => {
+        $crate::solana_program_stubs::declare_sol_app_stubsv2!(entry);
+    };
+}
