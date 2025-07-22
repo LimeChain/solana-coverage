56136955420b1f7481584a7f906f9b1599df706c
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2025-07-22 08:45:20 +0300

    Remove the first stubs api. Some comments.

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,11 @@
 #[allow(unused_imports)]
 pub use solana_program_stubs;
 
-#[macro_export]
-#[cfg(not(target_os = "solana"))]
-macro_rules! coverage {
-    ($entry_function:expr) => {
-        $crate::solana_program_stubs::declare_sol_app_stubs!($entry_function);
-    };
-}
-
+/// A macro providing the necessary stubs for a Solana program.
 #[macro_export]
 #[cfg(not(target_os = "solana"))]
 macro_rules! anchor_coverage {
     () => {
-        $crate::solana_program_stubs::declare_sol_app_stubs!(entry);
-    };
-}
-
-#[macro_export]
-#[cfg(not(target_os = "solana"))]
-macro_rules! coveragev2 {
-    ($entry_function:expr) => {
-        $crate::solana_program_stubs::declare_sol_app_stubsv2!($entry_function);
-    };
-}
-
-#[macro_export]
-#[cfg(not(target_os = "solana"))]
-macro_rules! anchor_coveragev2 {
-    () => {
-        $crate::solana_program_stubs::declare_sol_app_stubsv2!(entry);
+        $crate::solana_program_stubs::declare_sol_app_stubs!();
     };
 }
